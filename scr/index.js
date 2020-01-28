d1ed442ef1597dc1ecb95c4b5b720100a2a9c7ba
garciamartinezea <38872916+garciamartinezea@users.noreply.github.com>
2020-01-28 10:07:20 -0500

    begin

---
+++ scr/index.js
@@ -0,0 +1,11 @@
+// import './cpt-meta';
+
+
+// console.log("HIHIH")
+
+// import './blocks/core/column/index'
+// import './blocks/core/columns/index'
+import './blocks/core/image/index'
+
+import './uikit/index'
+// import './blocks/uikit/index'
